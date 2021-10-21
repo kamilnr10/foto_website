28bb3fe6c0ef786fa7e41a01a26457e91642cb0c
kamilnr10 <kamil.nr10@gmail.com>
2021-10-21 19:47:12 +0200

    feat:update Slider functionality in AboutMe section

--- components/organisms/GallerySlider/GallerySlider.js
+++ components/organisms/GallerySlider/GallerySlider.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Slider, LeftArrow, RightArrow } from './GallerySlider.styles';
+
+const Slide = styled.div`
+  opcity: 0;
+  transition-duration: 1s ease;
+
+  &.active {
+    opacity: 1;
+    transition-duration: 1s;
+    transform: scale(1.08);
+  }
+`;
 
 export const GallerySlider = ({
   slides,
@@ -13,14 +25,14 @@ export const GallerySlider = ({
       <RightArrow onClick={nextSlide} />
       {slides.map((slide, index) => {
         return (
-          <div
+          <Slide
             className={index === currentSlide ? 'slide active' : 'slide'}
             key={index}
           >
             {index === currentSlide && (
               <img src={slide.image} alt="street foto" />
             )}
-          </div>
+          </Slide>
         );
       })}
     </Slider>
--- components/organisms/Slider/Slider.js
+++ components/organisms/Slider/Slider.js
@@ -87,22 +87,59 @@ const Slider = (props) => {
   const getWidth = () => window.innerWidth;
 
   const [state, setState] = useState({
+    activeIndex: 0,
     translate: 0,
     transition: 0.45,
   });
 
-  const { translate, transition } = state;
+  const { translate, transition, activeIndex } = state;
+
+  const nextSlide = () => {
+    if (activeIndex === props.slides.length) {
+      return setState({
+        ...state,
+        translate: 0,
+        activeIndex: 0,
+      });
+    }
+
+    setState({
+      ...state,
+      translate: (activeIndex + 1) * getWidth(),
+      activeIndex: activeIndex + 1,
+    });
+  };
+
+  const prevSlide = () => {
+    if (activeIndex === 0) {
+      return setState({
+        ...state,
+        translate: (props.slides.length - 1) * getWidth(),
+        activeIndex: props.slides.length - 1,
+      });
+    }
+
+    setState({
+      ...state,
+      translate: (activeIndex - 1) * getWidth(),
+      activeIndex: activeIndex - 1,
+    });
+  };
+
   console.log(props.slides);
   return (
     <SliderWrapper>
       <SliderContent
         translate={translate}
         transition={transition}
-        width={getWidth()}
-      />
-      {props.slides.map((slide, i) => (
-        <Slide key={slide + i} content={slide} />
-      ))}
+        width={getWidth() * props.slides.length}
+      >
+        {props.slides.map((slide, i) => (
+          <Slide key={slide + i} content={slide} />
+        ))}
+      </SliderContent>
+      <LeftArrow direction="left" handleClick={prevSlide} />
+      <RightArrow direction="right" handleClick={nextSlide} />
     </SliderWrapper>
   );
 };
